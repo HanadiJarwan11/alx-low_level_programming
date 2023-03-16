7a0bdc7f4582a038dc07b236af16c49d7ed07bc2
HanadiJarwan11 <hanadi-2003@yahoo.com>
2023-03-16 15:05:34 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-/*
+/**
  * main - prints programming is like  puzzle, followed by a new line
  * Return: returns 0
  */
-
 int main(void)
 {
 puts("Programming is like building a multilingual puzzle\n");
