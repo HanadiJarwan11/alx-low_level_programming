b251b6afe7267f352f4c2ee437aed83c06d9c386
HanadiJarwan11 <hanadi-2003@yahoo.com>
2023-03-16 12:54:17 -0700

    file

---
+++ 0x00-hello_world/trial.c
@@ -0,0 +1,8 @@
+/*first trial*/
+
+#include <stdio.h>
+int main(void)
+{
+printf("Helooo, this is Hanadis first trial");
+return(0);
+}
---
+++ 0x00-hello_world/trial2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("My name is %s and my age is %d\n","Hanadi",19);
+	return(0);
+}	
