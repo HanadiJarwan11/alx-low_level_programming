1b330c2f7cc2bdd45e0e9ee823a100321bb302b8
HanadiJarwan11 <hanadi-2003@yahoo.com>
2023-03-18 04:39:07 -0700

    pushing

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - stay positive
+ * Return: 0 (success)
+ */
+int main(void)
+	int n;
+{
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}
+	else if (n == 0)
+	{
+		printf("is zero\n");
+	}
+	else (n < 0)
+	{
+		printf("is negative\n");
+	}
+	return (0);
+}
