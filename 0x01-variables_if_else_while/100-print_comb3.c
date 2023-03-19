5eafd7c358b974555d97d33ea132a5cf633ddf11
HanadiJarwan11 <hanadi-2003@yahoo.com>
2023-03-19 14:48:29 -0700

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - possible combo of two digits
+ * Return:0 (success)
+ */
+int main(void)
+{
+	int x = 0;
+
+	while
+		(x < 99);
+	{
+		putchar (x + '0');
+		if (x < 99)
+		{
+			putchar (',');
+			putchar (' ');
+		}
+		x++;
+	}
+	putchar ('\n');
+
+	return (0);
+}
