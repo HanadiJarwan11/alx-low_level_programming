30e0db2259aa62c34bf6fc9c124181c3a9e14dd3
HanadiJarwan11 <hanadi-2003@yahoo.com>
2023-03-19 16:44:16 -0700

    second

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 /**
- * main - possible combo of three digits
- * Return:0 (success)
+ * main - possible combo of single digit numbers
+ * Return: 0 (success)
  */
 int main(void)
-
 {
-	int x = 0;
+    int x = 0;
 
-	while (x < 99);
+    while (x < 10)
+    {
+        putchar(x + '0');
+        if (x < 9)
+        {
+            putchar (',');
+                        putchar (x / 10 + '0');
+                        putchar (' ');
+                        x++;}
+    }
+    putchar('\n');
 
-	{
-		putchar (x + '0');
-		if (x < 10)
-		{
-			putchar (',');
-			putchar (x / 10 + '0');
-			putchar (' ');
-			x++;
-		}
-	}
-		putchar ('\n');
-
-		return (0);
+    return (0);
 }
