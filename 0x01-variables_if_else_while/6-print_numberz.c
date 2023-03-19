46be56bd8089b1cb34c19ab57f4cda2a8d807465
HanadiJarwan11 <hanadi-2003@yahoo.com>
2023-03-19 07:26:45 -0700

    start

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - base ten
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= 10; n++)
+		putchar(n);
+			putchar ('n');
+
+			return (0);
