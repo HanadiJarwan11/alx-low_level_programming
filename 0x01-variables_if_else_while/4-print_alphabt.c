cc4b490dd6a57d93a27ee076cd583433bacb29d9
HanadiJarwan11 <hanadi-2003@yahoo.com>
2023-03-19 05:28:09 -0700

    exclude

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,7 +7,8 @@ int main(void)
 {
 	char n;
 
-	for (n = 'a'; n <= 'd'; n <= 'f'; n <= 'p'; n <= 'r'; n <= 'z' ; n++)
+	for (n = 'a'; n <= 'z'; n++)
+		if (n != e && n != q)
 		putchar (n);
 	putchar('\n');
 
