1bf3c60daa05e8174c36f734d10b9c2f80caf78d
HanadiJarwan11 <hanadi-2003@yahoo.com>
2023-03-19 16:42:16 -0700

    last

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,6 +19,7 @@ int main(void)
 			putchar (' ');
 			x++;
 		}
+	}
 		putchar ('\n');
 
 		return (0);
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - possible combo of single digit numbers
+ * Return: 0 (success)
+ */
+int main(void)
+{
+    int x = 0;
+
+    while (x < 10)
+    {
+        putchar(x + '0');
+        if (x < 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+        x++;
+    }
+    putchar('\n');
+
+    return (0);
+}
