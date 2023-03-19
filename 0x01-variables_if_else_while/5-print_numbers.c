8380fb1f03252fe32cdfc25d8527af4bf37b2f21
HanadiJarwan11 <hanadi-2003@yahoo.com>
2023-03-19 08:17:04 -0700

    number

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,11 +4,12 @@
  * Return: 0 (success)
  */
 int main(void)
+{
+	char a;
 
-	int a;
-
-	for (a = '1'; a <= '10'; a++)
-	putchar (a)
+	for (a = '0'; a <= '9'; a++)
+	putchar (a);
+	putchar ('\n');
 
 	return (0);
-
+}
