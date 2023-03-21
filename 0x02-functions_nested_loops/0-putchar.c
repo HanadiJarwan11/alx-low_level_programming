2e47f400b4ec3d97a2629b38a05a2b46a4738694
HanadiJarwan11 <hanadi-2003@yahoo.com>
2023-03-21 12:14:02 -0700

    check

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - program that prints _putchar
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char n = "_putchar";
+
+	putchar (n);
+
+	putchar ('\n');
+
+	return (0);
+}
