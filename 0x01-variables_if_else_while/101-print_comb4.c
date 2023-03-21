f8bfac677d9d709767b8fc407e42545021ab6749
HanadiJarwan11 <hanadi-2003@yahoo.com>
2023-03-21 10:49:26 -0700

    comb

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,20 +7,20 @@ int main(void)
 {
 	int a, b, c;
 	
-	for (a = 0; a <= 9; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		for (b = 0; b <= 9; b++)
-		{
-			for (c = 0; c <= 9; c++)
-			{
-				if (a != b && a!=c && b!= c)
-                        {
-                                putchar (a + '0');
-                                putchar (b + '0');
-                                putchar (c + '0');
-                                putchar (' ');
-                        }
-                }
+	for (b = '0'; b <= '9'; b++)
+	{
+	for (c = '0'; c <= '9'; c++)
+	{
+	if (a != b && a != c && b != c)
+	{
+		putchar (a + '0');
+		putchar (b + '0');
+		putchar (c + '0');
+		putchar (' ');
+	}
+	}
         }
         }
         putchar ('\n');
