29528994b9255644b995f5d8684e2a15a2525ca7
HanadiJarwan11 <hanadi-2003@yahoo.com>
2023-03-31 02:06:04 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - returns the length of a string
+ * Return: nothing
+ * strlen - calculate the length of a string
+ * @s: pointer to _strlen
+ */
+int _strlen(char *s)
+{
+	_strlen(s);
+
+	return (0);
+}
