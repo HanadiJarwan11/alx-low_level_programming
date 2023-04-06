f86f0817e1dab0faba527c5bce516359478c75ba
HanadiJarwan11 <hanadi-2003@yahoo.com>
2023-04-06 16:06:24 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * fputs - write character c  to an unsigned char to stream
+ * @s: used as a character
+ * main - contains the function
+ * _puts_recursion - function that calls itself
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	printf();
+
+	return (0);
+}
